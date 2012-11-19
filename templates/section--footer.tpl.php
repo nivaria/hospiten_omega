6d3c2be565294c320378478ad4e620dd3ce068dc
dbrouard <diego@net-monsters.com>
2012-11-19 12:24:03 +0000

    layout home nuestros hospitales nuestros profesionales

---
+++ templates/section--footer.tpl.php
@@ -0,0 +1,7 @@
+<div<?php print $attributes; ?>>
+  <div class="layout_corner bottom_left_layout_corner" ></div>
+  <div class="layout_corner bottom_right_layout_corner" ></div>
+  <div id="footer_logo" ></div>
+  <div id="footer_club" ></div>
+  <?php print $content; ?>
+</div>
---
+++ templates/section--header.tpl.php
@@ -0,0 +1,5 @@
+<div<?php print $attributes; ?>>
+  <?php print $content; ?>
+  <div class="layout_corner top_left_layout_corner" ></div>
+  <div class="layout_corner top_right_layout_corner" ></div>
+</div>
