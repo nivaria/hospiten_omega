5ecc0836600a2bbbe5df1022dd19d20a6a346a08
dbrouard <diego@net-monsters.com>
2012-12-10 14:34:18 +0000

    feedback fixes

--- js/hospiten.js
+++ js/hospiten.js
@@ -5,4 +5,19 @@ jQuery(document).ready(function($){
     size=size-(number_of_normal_tabs*40);
     
     jQuery(".quicktabs-wrapper ul.quicktabs-tabs li").css("width",size/number_of_normal_tabs+"px");
+    
+    
+    jQuery(".view-centros .views-row.views-row-odd").each( function(){
+        var current=jQuery(this);
+        var next=current.next();
+        
+        if(current.height()>next.height()){
+            next.css("height",current.height()+"px");
+        } else {
+            current.css("height",next.height()+"px");
+        }
+    });
+    jQuery("body.node-type-especialidades .pruebas_especialidad h3 span").bind("click",function(){
+        jQuery("body.node-type-especialidades .pruebas_especialidad .field-name-field-pru-ser-especialidades").toggle();
+    });
 })
