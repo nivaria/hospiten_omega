aeda3fac7e319a8faabe95f278e651446e1ee6b8
dbrouard <diego@net-monsters.com>
2012-12-11 16:13:47 +0000

    styles

--- js/hospiten.js
+++ js/hospiten.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
     var size=688;
     var number_of_normal_tabs=jQuery(".quicktabs-wrapper ul.quicktabs-tabs li").not(".active").length;
     size=size-183;
-    size=size-(number_of_normal_tabs*40);
+    /*size=size-(number_of_normal_tabs*40);*/
     
     jQuery(".quicktabs-wrapper ul.quicktabs-tabs li").css("width",size/number_of_normal_tabs+"px");
     
