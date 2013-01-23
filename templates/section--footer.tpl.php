e71cbbec94d4feaa65af13e8cf5be29fa413889f
dbrouard <diego@net-monsters.com>
2013-01-23 00:38:07 +0000

    https://nivaria.jira.com/browse/HPTPORTAL-626

--- templates/section--footer.tpl.php
+++ templates/section--footer.tpl.php
@@ -2,6 +2,7 @@
   <div class="layout_corner bottom_left_layout_corner" ></div>
   <div class="layout_corner bottom_right_layout_corner" ></div>
   <div id="footer_logo" ></div>
+  <div id="footer_instituto" ></div>
   <div id="footer_club" ></div>
   <?php print $content; ?>
 </div>
