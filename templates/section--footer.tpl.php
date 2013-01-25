faea5e3b86a9ba26722f46065a7a61cc5292168b
dbrouard <diego@net-monsters.com>
2013-01-25 12:29:15 +0000

    * En el pie, quitar el logo de dialisis y poner enlace en el logo idis http://www.fundacionidis.com/

--- templates/section--footer.tpl.php
+++ templates/section--footer.tpl.php
@@ -1,8 +1,8 @@
 <div<?php print $attributes; ?>>
   <div class="layout_corner bottom_left_layout_corner" ></div>
-  <div class="layout_corner bottom_right_layout_corner" ></div>
+  <div class="layout_corner bottom_right_layout_corner" ></div>  
   <div id="footer_logo" ></div>
-  <div id="footer_instituto" ></div>
-  <div id="footer_club" ></div>
+  <a id="footer_instituto" target="blank" href="http://www.fundacionidis.com" ></a>
+  <!--div id="footer_club" ></div-->
   <?php print $content; ?>
 </div>
