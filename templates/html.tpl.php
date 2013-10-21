5a2b936f7ea730af68da02bda2ef9580f80c1cd0
Lucas Brembilla <lucashb92@gmail.com>
2013-10-21 21:58:47 +0200

    choosen plugin update

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -3,6 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
